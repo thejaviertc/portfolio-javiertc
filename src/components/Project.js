30c0afdab08aca4ca6bc13ba04998e4178fe6ace
thejaviertc <javiertcjobs@gmail.com>
2021-01-09 19:40:58 +0100

    Added new Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 
 // Images
+import twitchImg from "../img/projects/twitch_data.jpg";
 import steamStatsImg from "../img/projects/steamstats.jpg";
 import emergenciasImg from "../img/projects/emergencies.jpg";
 
@@ -16,6 +17,7 @@ class Project extends Component {
             <section id="projects" className="bg-primary py-5">
                 <div className="container">
                     <h2 className="text-center">Proyectos<Hr width="280px" /></h2>
+                    <ProjectItem position='0' title="Twitch Data" desc="Web hecha para que la gente introduzca a un Streamer de Twitch, y le devuelva información sobre el streamer, como sus seguidores, el tipo de streamer..." badge="ReactJS" type="warning" link="https://github.com/thejaviertc/twitch-data" image={twitchImg} />
                     <ProjectItem position='1' title="Steam Workshop Stats" desc="Web hecha para que la gente introduzca su perfil de Steam, y le devuelva una lista con todos los addons que hayan publicado y sus estadísticas, como las visitas, favoritos... Además podrá ver las estadísticas totales." badge="NodeJS" type="success" link="https://github.com/thejaviertc/steam-workshop-stats" image={steamStatsImg} />
                     <ProjectItem position='0' title="Vehículos de Emergencias" desc="Vehículos de Emergencias de España, para que las comunidades de Garry's Mod de Roleplay tengan una gran variedad de vehículos de emergencias españoles." badge="Diseño" type="primary" link="https://steamcommunity.com/id/javiertc/myworkshopfiles/?appid=4000" image={emergenciasImg} />
                 </div>
