5bc7e41a82e93fdcf0eeeeaeac831816af1fd26b
thejaviertc <javier.toribiocouz@gmail.com>
2021-08-28 15:55:10 +0200

    Better Project Images

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,6 @@
 // Basic
 import React, { Component } from "react";
 
-// Images
-import twitchImg from "../img/projects/twitch_data.jpg";
-import steamStatsImg from "../img/projects/steamstats.jpg";
-
 // Components
 import Hr from "./Hr";
 import ProjectItem from "./ProjectItem";
@@ -16,8 +12,8 @@ class Project extends Component {
             <section id="projects" className="bg-primary py-5">
                 <div className="container">
                     <h2 className="text-center">Projects<Hr width="240px" /></h2>
-                    <ProjectItem position='0' title="Twitch Data" desc="Website made for people to introduce a Twitch Streamer, and return information about the streamer, such as his followers, the type of streamer, statistics of his videos..." badge="ReactJS" type="warning" link="https://github.com/thejaviertc/twitch-data" image={twitchImg} />
-                    <ProjectItem position='1' title="Steam Workshop Stats" desc="Website made for people to enter their Steam profile, and return a list with all the addons they have published and their statistics, such as visits, favorites ... You can also see the total statistics." badge="NodeJS" type="success" link="https://github.com/thejaviertc/steam-workshop-stats" image={steamStatsImg} />
+                    <ProjectItem position='0' title="Twitch Data" desc="Website made for people to introduce a Twitch Streamer, and return information about the streamer, such as his followers, the type of streamer, statistics of his videos..." badge="ReactJS" type="warning" link="https://github.com/thejaviertc/twitch-data" image="https://raw.githubusercontent.com/thejaviertc/portfolio-javiertc/main/src/img/github/twitch-data.jpg" />
+                    <ProjectItem position='1' title="Steam Workshop Stats" desc="Website made for people to enter their Steam profile, and return a list with all the addons they have published and their statistics, such as visits, favorites ... You can also see the total statistics." badge="NodeJS" type="success" link="https://github.com/thejaviertc/steam-workshop-stats" image="https://raw.githubusercontent.com/thejaviertc/portfolio-javiertc/main/src/img/github/steam-workshop-stats.jpg" />
                 </div>
             </section>
         );
