afc68741cc25aaa318a71e9a0de3dba93ef592f4
thejaviertc <javier.toribiocouz@gmail.com>
2022-06-01 23:28:38 +0200

    Reorganizated Bootstrap & SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
-import "./App.scss";
 import Home from "./pages/Home";
 
 export default function App() {
