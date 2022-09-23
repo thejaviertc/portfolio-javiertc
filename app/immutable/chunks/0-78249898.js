0c8d560192130762a13dc1af86f6329d500994d3
thejaviertc <45662206+thejaviertc@users.noreply.github.com>
2022-09-23 21:16:57 +0000

    Deploying to gh-pages from @ thejaviertc/portfolio-javiertc@e40f55b6e059b1b96ac2364615db15f7f8aae6bc 🚀

---
+++ app/immutable/chunks/0-78249898.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-c9a66c0f.js";import{default as t}from"../components/pages/_layout.svelte-52c89d17.js";export{t as component,r as shared};
---
+++ app/immutable/chunks/2-c40aa1c2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-49451e00.js";export{t as component};
