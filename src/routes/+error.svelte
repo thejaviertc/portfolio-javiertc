b290372a4117b0899f457a6bbb9e13024c5fc423
thejaviertc <javier.toribiocouz@gmail.com>
2022-12-18 00:15:29 +0100

    Better Error Page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,18 @@
 <script>
 	import { page } from "$app/stores";
+	import Button from "$lib/Button.svelte";
+	import { faBackward } from "@fortawesome/free-solid-svg-icons";
 </script>
 
 <svelte:head>
 	<title>{$page.status}</title>
 </svelte:head>
 
-<h1 class="text-center">{$page.status}</h1>
+<div class="bg-white dark:bg-zinc-900 min-h-screen flex justify-center items-center">
+	<div>
+		<h2 class="dark:text-gray-100">Something Happened - {$page.status}</h2>
+		<div class="flex justify-center my-6">
+			<Button faIcon={faBackward} link="/" text="Return Back" />
+		</div>
+	</div>
+</div>
