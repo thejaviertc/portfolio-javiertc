30b04c62b58b4560024e334e6a8721c6e882c2a7
thejaviertc <javier.toribiocouz@gmail.com>
2023-03-28 20:28:19 +0200

    Add DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,19 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				portfolio: {
+					"primary": "#191E29",
+					"secondary": "#02A879",
+					"accent": "#FF0000",
+					"neutral": "#696E79",
+					"base-100": "#ffffff",
+				},
+			},
+		],
+	},
+	safelist: ["bg-primary", "bg-secondary", "btn-primary", "btn-secondary"],
 };
