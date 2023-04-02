2fad8a475e1741be281e4e38dadd4828dad4b981
thejaviertc <javier.toribiocouz@gmail.com>
2023-04-02 19:27:52 +0200

    Fix grid gap

--- src/lib/KnowledgeGroup.svelte
+++ src/lib/KnowledgeGroup.svelte
@@ -8,8 +8,8 @@
 
 <div class="bg-secondary py-8 text-center rounded-xl shadow-xl">
 	<h1><Fa icon={faIcon} /></h1>
-	<h2 class="my-6">{title}</h2>
-	<div class="grid grid-cols-2 gap-2 mx-10">
+	<h2 class="my-8">{title}</h2>
+	<div class="grid grid-cols-2 gap-4 mx-10">
 		<slot />
 	</div>
 </div>
