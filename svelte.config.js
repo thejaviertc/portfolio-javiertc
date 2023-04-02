762a483e632a6459020f34ea00f4a3390ce3760c
thejaviertc <javier.toribiocouz@gmail.com>
2023-04-02 22:54:11 +0200

    Fix CSS and JS not loading in production

--- svelte.config.js
+++ svelte.config.js
@@ -6,14 +6,7 @@ const config = {
 	preprocess: vitePreprocess({ postcss: true }),
 	kit: {
 		appDir: "app",
-		adapter: adapter({
-			pages: "build",
-			assets: "build",
-			fallback: null,
-		}),
-		paths: {
-			base: process.env.NODE_ENV === "development" ? "" : "/portfolio-javiertc",
-		},
+		adapter: adapter(),
 	},
 };
 
