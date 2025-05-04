a856756e71d9caf901e77b1e6e54dd46fd23670f
thejaviertc <45662206+thejaviertc@users.noreply.github.com>
2025-05-04 10:01:08 +0000

    Deploying to gh-pages from @ thejaviertc/portfolio-javiertc@ece8d9a89955046debba677338c204d16c2e231d 🚀

---
+++ app/immutable/chunks/COeroCgx.js
@@ -0,0 +1,1 @@
+var t;const e=((t=globalThis.__sveltekit_ry7gxf)==null?void 0:t.base)??"/portfolio-javiertc";var s;const a=((s=globalThis.__sveltekit_ry7gxf)==null?void 0:s.assets)??"https://thejaviertc.github.io/portfolio-javiertc";export{a,e as b};
